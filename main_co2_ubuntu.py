81314975be0d320b69f22619ab86e07855f61245
MaartendeNooijer <nooijermm@gmail.com>
2025-04-01 13:50:23 +0200

    Add Ubuntu-specific Python scripts

---
+++ main_co2_ubuntu.py
@@ -0,0 +1,240 @@
+import os
+from darts.tools.plot_darts import *
+from darts.tools.logging import redirect_all_output
+from model_co2_spe11b_ubuntu import ModelCCS  # NEW
+from darts.engines import redirect_darts_output
+
+def extract_elapsed_time(log_path, output_path):
+    """
+    Extracts the last occurrence of ELAPSED time from a log file and writes it to a text file.
+    """
+    elapsed_time = None
+    try:
+        with open(log_path, "r") as file:
+            lines = file.readlines()
+
+        for line in reversed(lines):
+            if "ELAPSED" in line:
+                parts = line.split("ELAPSED")
+                if len(parts) > 1:
+                    time_part = parts[1].strip().strip("()")
+                    if time_part.count(":") == 2:
+                        elapsed_time = time_part
+                        break
+
+        if elapsed_time:
+            with open(output_path, "w") as out:
+                out.write(elapsed_time)
+            print(f"Elapsed time '{elapsed_time}' written to {output_path}")
+        else:
+            print("No elapsed time found in the log file.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+def run(physics_type: str, case: str, out_dir: str, export_vtk=True, redirect_log=True, platform='cpu'):
+    #from darts.engines import set_num_threads
+    #import pandas as pd
+
+    # NT = int(os.getenv("OMP_NUM_THREADS", 5))
+    # set_num_threads(NT)
+    platform = 'gpu'
+    from darts.engines import set_gpu_device
+    set_gpu_device(1)  # Ensure the GPU is set to index 0
+    print('Platform =', platform)
+    print('Test started', 'physics_type:', physics_type, 'case:', case, 'platform=', platform)
+    os.makedirs(out_dir, exist_ok=True)
+    log_filename = os.path.join(out_dir, 'run.log')
+    if redirect_log:
+        log_stream = redirect_all_output(log_filename)
+
+    log_path = os.path.join(out_dir, "run_n.log")
+    redirect_darts_output(log_path)
+
+    m = ModelCCS()
+    m.physics_type = physics_type
+    m.set_input_data(case=case)
+    m.init_reservoir()
+    m.init(output_folder=out_dir, platform=platform)
+    m.save_data_to_h5(kind='solution')
+    m.set_well_controls()
+
+    ret = m.run_simulation(out_dir) #out_dir
+    if ret != 0:
+        exit(1)
+
+
+    # if export_vtk:
+    #     m.reservoir.create_vtk_wells(output_directory=out_dir)
+    #     steps_to_export = [0] + list(range(3, len(m.idata.sim.time_steps), 4)) + [len(m.idata.sim.time_steps) - 1]
+    #     for ith_step in steps_to_export:
+    #         m.output_to_vtk(ith_step=ith_step)
+
+    def add_columns_time_data(time_data):
+        molar_mass_co2 = 44.01  # kg/kmol
+        time_data['Time (years)'] = time_data['time'] / 365.25
+        for k in list(time_data.keys()):
+            if physics_type == 'ccs' and 'V rate (m3/day)' in k:
+                time_data[k.replace('V rate (m3/day)', 'V rate (kmol/day)')] = time_data[k]
+                time_data[k.replace('V rate (m3/day)', 'V rate (ton/day)')] = time_data[k] * molar_mass_co2 / 1000
+                time_data.drop(columns=k, inplace=True)
+            if physics_type == 'ccs' and 'V  volume (m3)' in k:
+                time_data[k.replace('V  volume (m3)', 'V volume (kmol)')] = time_data[k]
+                time_data[k.replace('V  volume (m3)', 'V volume (Mt/year)')] = time_data[k] * molar_mass_co2 / 1000 / 1e6
+                time_data.drop(columns=k, inplace=True)
+
+    import pandas as pd
+    time_data = pd.DataFrame.from_dict(m.physics.engine.time_data)
+    add_columns_time_data(time_data)
+    time_data.to_pickle(os.path.join(out_dir, 'time_data.pkl'))
+
+    time_data_report = pd.DataFrame.from_dict(m.physics.engine.time_data_report)
+    add_columns_time_data(time_data_report)
+    time_data_report.to_pickle(os.path.join(out_dir, 'time_data_report.pkl'))
+
+    writer = pd.ExcelWriter(os.path.join(out_dir, 'time_data.xlsx'))
+    time_data.to_excel(writer, sheet_name='time_data')
+    writer.close()
+
+    press_gridcells = time_data_report.filter(like='reservoir').columns.tolist()
+    chem_cols = time_data_report.filter(like='Kmol').columns.tolist()
+    time_data_report.drop(columns=press_gridcells + chem_cols, inplace=True)
+    time_data_report['Time (years)'] = time_data_report['time'] / 365.25
+    writer = pd.ExcelWriter(os.path.join(out_dir, 'time_data_report.xlsx'))
+    time_data_report.to_excel(writer, sheet_name='time_data_report')
+    writer.close()
+
+    m.reservoir.centers_to_vtk(out_dir)
+    m.print_timers()
+
+    if export_vtk:
+        # read h5 file and write vtk
+        m.reservoir.create_vtk_wells(output_directory=out_dir)
+        for ith_step in range(len(m.idata.sim.time_steps)):
+            m.output_to_vtk(ith_step=ith_step)
+
+    # Optional: Remove large files
+    for fname in ['solution.h5', 'well_data.h5']:
+        fpath = os.path.join(out_dir, fname)
+        if os.path.exists(fpath):
+            os.remove(fpath)
+            print(f"Deleted {fpath}")
+
+    extract_elapsed_time(log_path, os.path.join(out_dir, "elapsed_time.txt"))
+
+    return time_data, time_data_report, m.idata.well_data.wells.keys(), m.well_is_inj
+
+
+##########################################################################################################
+def plot_results(wells, well_is_inj, time_data_list, time_data_report_list, label_list, physics_type, out_dir):
+    plt.rc('font', size=12)
+
+    def plot_total_inj_gas_rate_darts_volume(darts_df, style='-', color='#00A6D6', ax=None, alpha=1):
+        acc_df = pd.DataFrame()
+        acc_df['time'] = darts_df['time']/365.25
+        acc_df['total'] = 0
+
+        found_cols = []
+        # Match any injector column with Mt/year volume
+        for col in darts_df.columns:
+            if "INJ" in col and "V volume (Mt/year)" in col:
+                found_cols.append(col)
+                acc_df['total'] += darts_df[col]
+
+        ax = acc_df.plot(x='time', y='total', style='o-', color=color,
+                         ax=ax, alpha=alpha)
+
+        ymin, ymax = ax.get_ylim()
+        if ymax < 0:
+            ax.set_ylim(ymin * 1.1, 0)
+
+        if ymin > 0:
+            ax.set_ylim(0, ymax * 1.1)
+
+        return ax
+
+    for well_name in wells:
+
+        ax = None
+        for time_data, label in zip(time_data_report_list, label_list):
+            ax = plot_total_inj_gas_rate_darts_volume(time_data, ax=ax)  # , label=label) #NEW was ax = plot_total_inj_gas_rate_darts(time_data, ax=ax)
+        ax.set(xlabel="Years", ylabel="Total Inj Gas Rate [MT/Year]")
+        plt.tight_layout()
+        plt.savefig(os.path.join(out_dir, 'total_inj_gas_rate_' + well_name + '_' + case + '.png'))
+        plt.close()
+
+    for well_name in wells:
+        ax = None
+        for time_data_report, label in zip(time_data_list, label_list): #NEW was time_data_report_list to check per year
+            ax = plot_bhp_darts(well_name, time_data_report, ax=ax)#, label=label)
+        ax.set(xlabel="Days", ylabel="BHP [bar]")
+        plt.savefig(os.path.join(out_dir, 'well_' + well_name + '_bhp_' + case + '.png'))
+        plt.tight_layout()
+        plt.close()
+
+
+if __name__ == '__main__':
+    platform = 'gpu' if os.getenv('TEST_GPU') == '1' else 'cpu'
+
+    physics_list = ['ccs']
+    cases_list = []
+    cases_list += ['grid_CCS_maarten'] #Heterogeneous, contains different regions
+    #cases_list += ['grid_CCS_maarten_homogeneous']  # Homogeneous, one region assignment
+
+    # cases_list += ["case_1_50x50x40"]
+    # cases_list += ["case_1_100x100x80"]
+    # cases_list += ["case_1_125x125x80"]
+    # cases_list += ["case_1_250x250x80"]
+    # cases_list += ["case_2_50x50x40"]
+    # cases_list += ["case_2_100x100x80"]
+    # cases_list += ["case_2_125x125x80"]
+    # cases_list += ["case_2_250x250x80"]
+    #cases_list += ["case_3_50x50x40"]
+    #cases_list += ["case_3_100x100x80"]
+    # cases_list += ["case_3_125x125x80"]
+    # cases_list += ["case_3_250x250x80"]
+
+    well_controls = []
+    well_controls +=  ['wbhp']
+    #well_controls += ['rate']
+
+    run_tag = "_UBUNTU"
+
+    for physics_type in physics_list:
+        for case_geom in cases_list:
+            for wctrl in well_controls:
+                if physics_type == 'deadoil' and wctrl == 'wrate':
+                    continue
+                case = case_geom + '_' + wctrl
+                folder_name = 'results_' + physics_type + '_' + case + run_tag
+                out_dir = os.path.join("results", folder_name)
+
+                time_data, time_data_report, wells, well_is_inj = run(
+                    physics_type=physics_type,
+                    case=case,
+                    out_dir=out_dir,
+                    redirect_log=False,
+                    export_vtk=True,
+                    platform=platform
+                )
+
+                # one can read well results from pkl file to add/change well plots without re-running the model
+                pkl1_dir = '..'
+                pkl_fname = 'time_data.pkl'
+                pkl_report_fname = 'time_data_report.pkl'
+                time_data_list = [time_data]
+                time_data_report_list = [time_data_report]
+                label_list = [None]
+
+                # compare the current results with another run
+                # pkl1_dir = r'../../../open-darts_dev/models/cpg_sloping_fault/results_' + physics_type + '_' + case_geom
+                # time_data_1 = pd.read_pickle(os.path.join(pkl1_dir, pkl_fname))
+                # time_data_report_1 = pd.read_pickle(os.path.join(pkl1_dir, pkl_report_fname))
+                # time_data_list = [time_data_1, time_data]
+                # time_data_report_list = [time_data_report_1, time_data_report]
+                # label_list = ['1', 'current']
+
+                plot_results(wells=wells, well_is_inj=well_is_inj,
+                             time_data_list=time_data_list, time_data_report_list=time_data_report_list,
+                             label_list=label_list,
+                             physics_type=physics_type, out_dir=out_dir)
---
+++ model_co2_spe11b_ubuntu.py
@@ -0,0 +1,393 @@
+import numpy as np
+
+from darts.input.input_data import InputData
+from darts.models.darts_model import DartsModel
+from set_case import set_input_data
+from darts.engines import value_vector
+
+from darts.physics.super.physics import Compositional
+from darts.physics.super.property_container import PropertyContainer
+
+from darts.physics.properties.basic import PhaseRelPerm, ConstFunc
+from darts.physics.properties.density import Garcia2001
+from darts.physics.properties.viscosity import Fenghour1998, Islam2012
+from darts.physics.properties.eos_properties import EoSDensity, EoSEnthalpy
+
+from dartsflash.libflash import NegativeFlash
+from dartsflash.libflash import CubicEoS, AQEoS, FlashParams, InitialGuess
+from dartsflash.components import CompData
+from model_cpg import Model_CPG, fmt
+from model_cpg_ubuntu import Model_CPG, fmt
+
+from dataclasses import dataclass, field
+
+from scipy.special import erf
+
+# region Dataclasses
+@dataclass
+class Corey:
+    nw: float
+    ng: float
+    swc: float
+    sgc: float
+    krwe: float
+    krge: float
+    labda: float
+    p_entry: float
+    pcmax: float
+    c2: float
+    def modify(self, std, mult):
+        i = 0
+        for attr, value in self.__dict__.items():
+            if attr != 'type':
+                setattr(self, attr, value * (1 + mult[i] * float(getattr(std, attr))))
+            i += 1
+
+    def random(self, std):
+        for attr, value in self.__dict__.items():
+            if attr != 'type':
+                std_in = value * float(getattr(std, attr))
+                param = np.random.normal(value, std_in)
+                if param < 0:
+                    param = 0
+                setattr(self, attr, param)
+
+class ModelCCS(Model_CPG):
+    def __init__(self):
+        self.zero = 1e-10
+        super().__init__()
+
+    def set_physics(self): #NEW
+        """Assign physical properties, including relative permeability, based on facies (SATNUM)."""
+        #Code used in SPE11b
+        """Physical properties"""
+        # Fluid components, ions and solid
+        components = ["H2O", "CO2"]
+        phases = ["V", "Aq"]
+        comp_data = CompData(components, setprops=True)
+
+        #nc = len(components)
+
+        flash_params = FlashParams(comp_data)
+
+        # EoS-related parameters
+        flash_params.add_eos("PR", CubicEoS(comp_data, CubicEoS.PR))
+        flash_params.add_eos("AQ", AQEoS(comp_data, {AQEoS.CompType.water: AQEoS.Jager2003,
+                                                     AQEoS.CompType.solute: AQEoS.Ziabakhsh2012,
+                                                     AQEoS.CompType.ion: AQEoS.Jager2003
+                                                     }))
+        pr = flash_params.eos_params["PR"].eos
+        aq = flash_params.eos_params["AQ"].eos
+
+        flash_params.eos_order = ["PR", "AQ"]
+
+        # Initialize physics model
+        self.physics = Compositional(
+            components=components,
+            phases=phases,
+            timer=self.timer,
+            n_points=self.idata.obl.n_points,
+            min_p=self.idata.obl.min_p,
+            max_p=self.idata.obl.max_p,
+            min_z=self.idata.obl.min_z,
+            max_z=self.idata.obl.max_z,
+            min_t=self.idata.obl.min_t,
+            max_t=self.idata.obl.max_t,
+            thermal=self.idata.obl.thermal,
+            cache=self.idata.obl.cache
+        )
+
+        self.physics.n_axes_points[0] = 101  # sets OBL points for pressure, From SPE11b
+
+        temperature = None  # if None, then thermal=True
+
+        facies_rel_perm = {  # based on fluidflower paper
+            # # **Wells** ##NEW, values don't mind
+            0: Corey(nw=1.5, ng=1.5, swc=0.10, sgc=0.10, krwe=1.0, krge=1.0, labda=2., p_entry=0, pcmax=0.1, c2=1.5),
+            # **Channel Sand (Facies 1)**
+            1: Corey(nw=1.5, ng=1.5, swc=0.10, sgc=0.10, krwe=1.0, krge=1.0, labda=2., p_entry=0.025602, pcmax=300, c2=1.5),  #p_entry=0.025602
+            # **Overbank Sand (Facies 2)**
+            2: Corey(nw=1.5, ng=1.5, swc=0.12, sgc=0.10, krwe=1.0, krge=1.0, labda=2., p_entry=0.038706, pcmax=300, c2=1.5),  # p_entry=0.038706
+            # **Shale (Facies 3)**
+            3: Corey(nw=1.5, ng=1.5, swc=0.32, sgc=0.10, krwe=1.0, krge=1.0, labda=2., p_entry=1.935314, pcmax=300, c2=1.5)}  # p_entry=1.935314
+
+
+        # Assign properties per facies using `add_property_region`
+        for i, (region, corey_params) in enumerate(facies_rel_perm.items()): #NEW
+
+            #Original
+            property_container = AlternativeContainer(phases_name=phases, components_name=components,Mw=comp_data.Mw[:2], #NEW, was property_container = PropertyContainer(phases_name=phases, components_name=components,Mw=comp_data.Mw,
+                                                   temperature=temperature, min_z=self.zero/10)
+
+            property_container.flash_ev = NegativeFlash(flash_params, ["PR", "AQ"], [InitialGuess.Henry_VA])
+
+            property_container.density_ev = dict([('V', EoSDensity(eos=pr, Mw=comp_data.Mw[:2])),
+                                                  ('Aq', Garcia2001(components)), ])
+
+            property_container.viscosity_ev = dict([('V', Fenghour1998()),
+                                                    ('Aq', Islam2012(components)), ])
+
+            property_container.enthalpy_ev = dict([('V', EoSEnthalpy(eos=pr)),('Aq', EoSEnthalpy(eos=aq)),])
+
+
+            property_container.conductivity_ev = dict([('V', ConstFunc(8.4)),('Aq', ConstFunc(170.)), ])
+
+
+            property_container.rel_perm_ev = dict([('V', ModBrooksCorey(corey_params, 'V')),  # interesting
+                                                   ('Aq', ModBrooksCorey(corey_params, 'Aq'))])  # interesting
+
+            property_container.capillary_pressure_ev = CapillaryPressure(corey_params)  # interesting
+
+            self.physics.add_property_region(property_container, i)  # interesting
+
+            property_container.output_props = {"satV": lambda ii=i: self.physics.property_containers[ii].sat[0],
+                                               "satA": lambda ii=i: self.physics.property_containers[ii].sat[1],
+                                               "xCO2": lambda ii=i: self.physics.property_containers[ii].x[1, 1],
+                                               "yCO2": lambda ii=i: self.physics.property_containers[ii].x[0, 1],
+                                               "xH20": lambda ii=i: self.physics.property_containers[ii].x[1, 0],
+                                               "yH20": lambda ii=i: self.physics.property_containers[ii].x[0, 0],
+                                               "enthV": lambda ii=i: self.physics.property_containers[ii].enthalpy[0], #was 0
+                                               "Pc": lambda ii=i: self.physics.property_containers[ii].pc[1]}
+
+        return
+
+
+    def get_arrays(self, ith_step):
+        '''
+        :return: dictionary of current unknown arrays (p, T)
+        '''
+        # Find index of properties to output
+        ev_props = self.physics.property_operators[next(iter(self.physics.property_operators))].props_name
+        # If output_properties is None, all variables and properties from property_operators will be passed
+        props_names = list(ev_props)
+        props_names = props_names
+
+        timesteps, property_array = self.output_properties(output_properties=props_names, timestep=ith_step)
+
+        return property_array
+
+    def get_arrays_gredcl(self, ith_step):
+        '''
+        :return: dictionary of current unknown arrays (p, T)
+        '''
+        # Find index of properties to output
+        ev_props = self.physics.property_operators[next(iter(self.physics.property_operators))].props_name
+        # If output_properties is None, all variables and properties from property_operators will be passed
+        props_names = list(ev_props)
+        props_names = props_names
+
+        timesteps, property_array = self.output_properties(output_properties=props_names, timestep=ith_step)
+
+        # Assume these are your values
+        specgrid = self.reservoir.dims
+        coord = self.reservoir.coord  # Should be a NumPy array of shape (4056,)
+        zcorn = self.reservoir.zcorn  # Should be a NumPy array of shape (200000,)
+
+        from collections import OrderedDict
+
+        # Create a new ordered dict, starting with dims, coord, zcorn
+        ordered_property_array = OrderedDict()
+
+        ordered_property_array['SPECGRID'] = specgrid
+        ordered_property_array['COORD'] = coord
+        ordered_property_array['ZCORN'] = zcorn
+
+        # Add the rest of your properties from the original dict
+        for key, value in property_array.items():
+            ordered_property_array[key] = value
+
+        return ordered_property_array
+
+    def set_input_data(self, case=''):
+        self.idata = InputData(type_hydr='thermal', type_mech='none', init_type='uniform')
+        set_input_data(self.idata, case)
+        self.idata.faultfile =  self.idata.faultfile #NEW
+
+        self.idata.geom.burden_layers = 0
+
+        # well controls
+        wdata = self.idata.well_data
+        wells = wdata.wells  # short name
+        # set default injection composition
+        #wdata.inj = value_vector([self.zero])  # injection composition - water
+        y2d = 365.25
+
+        rate_kg_year = 2e9 #0.1 Mt
+        rate_kg_day = rate_kg_year/y2d
+        kg_to_mol = 44.01 #kg/kmol
+        rate_kmol_day = rate_kg_day/kg_to_mol
+
+        if 'wbhp' in case:
+            print("The string 'wbhp' is found in case!")
+            for w in wells:
+                wdata.add_inj_bhp_control(name=w, bhp=172, comp_index=1, temperature=300)  # kmol/day | bars | K
+                #wdata.add_prd_rate_control(time=10 * y2d, name=w, rate=0., comp_index=0, bhp_constraint=70)  # STOP WELL
+        elif 'wrate' in case:
+            for w in wells:
+                wdata.add_inj_rate_control(name=w, rate=rate_kmol_day, comp_index=1, bhp_constraint=190, temperature=300) #rate=5e5/8 is approximately 1 Mt per year #was rate = 6e6 # kmol/day | bars | K
+                #wdata.add_prd_rate_control(time=10 * y2d, name=w, rate=0., comp_index=0, bhp_constraint=70)  # STOP WELL
+
+        self.idata.obl.n_points = 1000
+        self.idata.obl.zero = 1e-11
+        self.idata.obl.min_p = 1.
+        self.idata.obl.max_p = 400.
+        self.idata.obl.min_t = 273.15
+        self.idata.obl.max_t = 373.15
+        self.idata.obl.min_z = self.idata.obl.zero
+        self.idata.obl.max_z = 1 - self.idata.obl.zero
+        self.idata.obl.cache = False
+        self.idata.obl.thermal = True #True #This sets case to non-isothermal
+
+    def set_initial_conditions(self):
+        self.temperature_initial_ = 273.15 + 76.85  # K
+        self.initial_values = {"pressure": 100.,
+                            "H2O": 0.99995,
+                            "temperature": self.temperature_initial_
+                            }
+        super().set_initial_conditions()
+
+        mesh = self.reservoir.mesh
+        depth = np.array(mesh.depth, copy=True) #- 2000
+        # set initial pressure
+        pressure_grad = 100
+        pressure = np.array(mesh.pressure, copy=False)
+        pressure[:] = depth[:pressure.size] / 1000 * pressure_grad + 1
+        # set initial temperature
+        temperature_grad = 30
+        temperature = np.array(mesh.temperature, copy=False)
+        temperature[:] = depth[:pressure.size] / 1000 * temperature_grad + 273.15 + 20
+        #temperature[:] = 350 #NEW #Deactivated to make sure that I have temperature gradient include
+
+    def set_well_controls(self, time: float = 0., verbose=True):
+        '''
+        :param time: simulation time, [days]
+        :return:
+        '''
+        inj_stream_base = [self.zero * 100]
+        eps_time = 1e-15
+        for w in self.reservoir.wells:
+            # find next well control in controls list for different timesteps
+            wctrl = None
+            for wctrl_t in self.idata.well_data.wells[w.name].controls:
+                if np.fabs(wctrl_t[0] - time) < eps_time:  # check time
+                    wctrl = wctrl_t[1]
+                    break
+            if wctrl is None:
+                continue
+            if wctrl.type == 'inj':  # INJ well
+                inj_stream = inj_stream_base
+                if self.physics.thermal:
+                    inj_stream += [wctrl.inj_bht]
+                if wctrl.mode == 'rate': # rate control
+                    w.control = self.physics.new_rate_inj(wctrl.rate, inj_stream, wctrl.comp_index)
+                    w.constraint = self.physics.new_bhp_inj(wctrl.bhp_constraint, inj_stream)
+                elif wctrl.mode == 'bhp': # BHP control
+                    w.control = self.physics.new_bhp_inj(wctrl.bhp, inj_stream)
+                else:
+                    print('Unknown well ctrl.mode', wctrl.mode)
+                    exit(1)
+            elif wctrl.type == 'prod':  # PROD well
+                if wctrl.mode == 'rate': # rate control
+                    w.control = self.physics.new_rate_prod(wctrl.rate, wctrl.comp_index)
+                    w.constraint = self.physics.new_bhp_prod(wctrl.bhp_constraint)
+                elif wctrl.mode == 'bhp': # BHP control
+                    w.control = self.physics.new_bhp_prod(wctrl.bhp)
+                else:
+                    print('Unknown well ctrl.mode', wctrl.mode)
+                    exit(1)
+            else:
+                print('Unknown well ctrl.type', wctrl.type)
+                exit(1)
+            if verbose:
+                print('set_well_controls: time=', time, 'well=', w.name, w.control, w.constraint)
+
+        # check
+        for w in self.reservoir.wells:
+            assert w.control is not None, 'well control is not initialized for the well ' + w.name
+            if verbose and w.constraint is not None and 'rate' in str(type(w.control)):
+                print('A constraint for the well ' + w.name + ' is not initialized!')
+
+
+class ModBrooksCorey:
+    def __init__(self, corey, phase):
+
+        self.phase = phase
+
+        if self.phase == "Aq":
+            self.k_rw_e = corey.krwe
+            self.swc = corey.swc
+            self.sgc = 0
+            self.nw = corey.nw
+        else:
+            self.k_rg_e = corey.krge
+            self.sgc = corey.sgc
+            self.swc = 0
+            self.ng = corey.ng
+
+    def evaluate(self, sat):
+        if self.phase == "Aq":
+            Se = (sat - self.swc)/(1 - self.swc - self.sgc)
+            if Se > 1:
+                Se = 1
+            elif Se < 0:
+                Se = 0
+            k_r = self.k_rw_e * Se ** self.nw
+        else:
+            Se = (sat - self.sgc) / (1 - self.swc - self.sgc)
+            if Se > 1:
+                Se = 1
+            elif Se < 0:
+                Se = 0
+            k_r = self.k_rg_e * Se ** self.ng
+
+        return k_r
+
+class CapillaryPressure:
+    def __init__(self, corey):
+        self.nph = 2
+        self.swc = corey.swc
+        self.p_entry = corey.p_entry
+        self.labda = corey.labda
+        self.eps = 1e-3
+
+    def evaluate(self, sat):
+        '''
+        default evaluator of capillary pressure Pc based on pow
+        :param sat: saturation
+        :return: Pc
+        '''
+        if self.nph > 1:
+            Se = (sat[1] - self.swc)/(1 - self.swc) #Here SAT[1], since first V, then AQ (propertycontainer) #Was Sat[0] for other script, but most likely should be sat[1] here
+            if Se < self.eps:
+                Se = self.eps
+            pc = self.p_entry * Se ** (-1/self.labda)
+
+            Pc = np.zeros(self.nph, dtype=object)
+            Pc[1] = pc #Shouldn't this be Pc[0]?
+        else:
+            Pc = [0.0]
+        return Pc #V, Aq
+
+class AlternativeContainer(PropertyContainer):
+    def run_flash(self, pressure, temperature, zc):
+        # Normalize fluid compositions
+        zc_norm = zc if not self.ns else zc[:self.nc_fl] / (1. - np.sum(zc[self.nc_fl:]))
+
+        # Evaluates flash, then uses getter for nu and x - for compatibility with DARTS-flash
+        error_output = self.flash_ev.evaluate(pressure, temperature, zc_norm)
+        flash_results = self.flash_ev.get_flash_results()
+        self.nu = np.array(flash_results.nu)
+        try:
+            self.x = np.array(flash_results.X).reshape(self.np_fl, self.nc_fl)
+        except ValueError as e:
+            print(e.args[0], pressure, temperature, zc)
+            error_output += 1
+
+        # Set present phase idxs
+        ph = np.array([j for j in range(self.np_fl) if self.nu[j] > 0], dtype=int)
+
+        if ph.size == 1:
+            self.x[ph[0]] = zc_norm
+
+        return ph
+
